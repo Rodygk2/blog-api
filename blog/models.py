c8e73091daa92b2d3ecdc9ce9aa73466fa601182
Rodygk2 <rodygk2@gmail.com>
2026-04-30 02:26:43 +0100

    feat: add immage upload to post

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     published = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='posts/', null=True, blank=True)
 
     def __str__(self):
         return self.title
